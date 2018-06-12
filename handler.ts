6a86be897f9c3e1e5360efd5c8e426d4075afdb3
ding <playdingnow@gmail.com>
2018-06-12 16:01:40 +0800

    feat: try to debug out chrome url

--- handler.ts
+++ handler.ts
@@ -1,4 +1,5 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import { getChromeUrl } from './lib/chrome';
 
 export const hello: Handler = (
   event: APIGatewayEvent,
@@ -10,6 +11,24 @@ export const hello: Handler = (
     body: JSON.stringify({
       message:
         'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  cb(null, response);
+};
+
+export const debug: Handler = async (
+  event: APIGatewayEvent,
+  context: Context,
+  cb: Callback
+) => {
+  const url = await getChromeUrl();
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      url,
       input: event,
     }),
   };
