214d85b7076279cea02c12f4737677b71f35617d
ding <playdingnow@gmail.com>
2018-06-14 15:24:16 +0800

    feat: rm hello method

--- handler.js
+++ handler.js
@@ -1,20 +1,5 @@
 const chrome = require('./lib/chrome');
 
-const hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 const debug = async (event, context, callback) => {
   const version = await chrome.version();
 
@@ -29,4 +14,4 @@ const debug = async (event, context, callback) => {
   callback(null, response);
 };
 
-module.exports = { hello, debug };
+module.exports = { debug };
