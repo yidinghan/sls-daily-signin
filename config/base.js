6e36e2253c871364c015c94813c1ab76492ab2d5
ding <playdingnow@gmail.com>
2018-06-14 16:01:14 +0800

    feat(chrome): add headless mode

--- config/base.js
+++ config/base.js
@@ -3,24 +3,12 @@ const gf = {
 };
 
 const chrome = {
-  flags: [
-    '--disable-background-networking',
-    '--disable-background-timer-throttling',
-    '--disable-client-side-phishing-detection',
-    '--disable-default-apps',
-    '--disable-dev-shm-usage',
-    '--disable-extensions',
-    '--disable-hang-monitor',
-    '--disable-popup-blocking',
-    '--disable-prompt-on-repost',
-    '--disable-sync',
-    '--disable-translate',
-    '--metrics-recording-only',
-    '--no-first-run',
-    '--safebrowsing-disable-auto-update',
-    '--enable-automation',
-    '--password-store=basic',
-    '--use-mock-keychain',
+  headless: true,
+  headlessFlags: [
+    '--headless',
+    '--disable-gpu',
+    '--hide-scrollbars',
+    '--mute-audio',
   ],
 };
 
