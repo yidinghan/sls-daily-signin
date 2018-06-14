283bd2def2231595dcb1744ee5122779d53d1a1c
ding <playdingnow@gmail.com>
2018-06-14 15:56:53 +0800

    feat(chrome): use puppeteer default args

--- config/base.js
+++ config/base.js
@@ -2,4 +2,26 @@ const gf = {
   jfhost: 'https://jf.gf.com.cn',
 };
 
-module.exports = { gf };
+const chrome = {
+  flags: [
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-client-side-phishing-detection',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-extensions',
+    '--disable-hang-monitor',
+    '--disable-popup-blocking',
+    '--disable-prompt-on-repost',
+    '--disable-sync',
+    '--disable-translate',
+    '--metrics-recording-only',
+    '--no-first-run',
+    '--safebrowsing-disable-auto-update',
+    '--enable-automation',
+    '--password-store=basic',
+    '--use-mock-keychain',
+  ],
+};
+
+module.exports = { gf, chrome };
