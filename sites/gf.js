63008bbc75afd328b85d2f640509a2ab2df9c3fc
ding <playdingnow@gmail.com>
2018-06-15 01:01:05 +0800

    feat(chrome): log page method

--- sites/gf.js
+++ sites/gf.js
@@ -2,10 +2,10 @@ const chrome = require('../lib/chrome');
 const { gf } = require('../config/base');
 
 const main = async () => {
-  const page = await chrome.page();
+  const page = await chrome.getPage();
   await page.goto(gf.jfhost);
 
-  const title = await page.title();
+  const { title } = await chrome.logPage(page);
 
   await page.browser().close();
   return title;
